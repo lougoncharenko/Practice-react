00138c7fb2de1d31571f3a1d963ac6a0522fde80
Louisa Goncharenko <louisa.goncharenko@shopify.com>
2022-11-21 11:43:26 -0500

    displaying person in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import {Person} from './components/Person'
 
 const App: React.FC = () => {
 
   return (
     <div className="App">
+      <Person name ='louisa' age ={25} email="louisa.gonch@gmail.com"/>
     </div>
   );
 }
