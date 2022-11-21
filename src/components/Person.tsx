1164194a37721206be5dfd08ab2a239e6762e956
Louisa Goncharenko <louisa.goncharenko@shopify.com>
2022-11-21 11:38:47 -0500

    added components

---
+++ src/components/Person.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+export interface Props {
+    name: string;
+    age: number;
+    email: string;
+}
+
+export const Person  = (props: Props) => {
+  return (
+    <div>
+      
+    </div>
+  );
+}
